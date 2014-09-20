259aa365692f3d0360331e0068d91ec1fdb4b1a6
jackeagle <jackeagle102@gmail.com>
2014-09-20 03:22:45 -0400

    AOGP-fication

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -222,7 +222,7 @@ def parse_device_from_folder(device):
 
 
 def parse_dependency_file(location):
-    dep_file = "cm.dependencies"
+    dep_file = "aogp.dependencies"
     dep_location = '/'.join([location, dep_file])
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
