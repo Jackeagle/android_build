9951e2e93db941b068e682ea2cc14bd26fa9e626
Jack <jackeagle102@gmail.com>
2014-09-29 23:33:21 +0530

    Set AOGP Device Info

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -43,6 +43,7 @@ if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
-echo "ro.cm.device=$CM_DEVICE"
+#AOGP Properties
+echo "ro.aogp.device=$AOGP_DEVICE"
 
 echo "# end build properties"
